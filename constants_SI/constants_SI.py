5ffb61e7aa2d8441ad4b30c513291eb35e8a77bb
Andréas Sundström <andsunds@student.chalmers.se>
2019-02-28 14:03:46 +0100

    This is a very simple library with the values of some relevant natural constants in SI units. There is probably something better out there, but I haven't looked.
    For the nerdy, the constants here are still in their pre-CGPM 2018 redefinition values.
    
     On branch master
     Changes to be committed:
    	new file:   __init__.py
    	new file:   constants_SI.py

---
+++ constants_SI/constants_SI.py
@@ -0,0 +1,23 @@
+#from math import pi
+
+PI=3.141592653589793
+pi=PI
+
+
+c=299792458          #m/s
+h=6.62606957e-34     #Js=kg m^2/s
+hbar=h/(2*pi)        #Js=kg m^2/s
+
+e=1.602176565e-19    #C
+m_e=9.10938291e-31   #kg
+m_p=1.672621777e-27  #kg
+
+mu_0=pi*4e-7         #A N
+eps_0=1/(mu_0*c*c)   #F/m
+epsilon_0=eps_0      #F/m
+
+k_B=1.3806488e-23    #J/K
+N_A=6.0221409e+23    #particles/mol
+N_Avogadro=N_A       #particles/mol
+
+G=6.67384e-11        #N m^2/kg^2 = m^3 /(kg s^2)
